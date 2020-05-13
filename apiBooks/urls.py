c54526a906c7337e28f9068ac84f8d741836ee2c
cgrountree <cgrountree@gmail.com>
2020-05-13 15:21:38 -0400

    add url

--- apiBooks/urls.py
+++ apiBooks/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('languages/', views.get_languages),
     path('mandarin/', views.get_mandarin_book),
     path('english/', views.get_english_book),
+    path('japanese/', views.get_japanese_book),
     # path('user-known-words/', views.get_user_known_words)
 ]
